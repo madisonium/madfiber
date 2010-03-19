43a6553410dbcc94dba63ca7f35228a88c78560a
Bradley Grzesiak <listrophy@gmail.com>
2010-03-18 23:24:12 -0400

    Fix copy on welcome page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+
+  def city_survey_url
+    'http://www.surveymonkey.com/s.aspx?sm=k%2bEqX3BaB6cqiHPoEZOq8kq9w15RSm%2bV19jRk46qcPc%3d&'
+  end
+
+  def facebook_url
+    'http://www.facebook.com/pages/Madison-WI/Bring-Google-Fiber-to-Madison-Wisconsin/298796674303'
+  end
+
+  def flickr_url
+    'http://www.flickr.com/groups/madfiber'
+  end
+
+  def google_landing_url
+    'http://www.google.com/appserve/fiberrfi'
+  end
+
+  def google_nomination_url
+    'http://www.google.com/appserve/fiberrfi/public/options'
+  end
+
+  def google_overview_url
+    'http://www.google.com/appserve/fiberrfi/public/overview'
+  end
+
+  def posterous_url
+    'http://madfiber.posterous.com'
+  end
 end
